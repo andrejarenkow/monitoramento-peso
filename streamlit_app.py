6c4a69d9a9a2dad89102f1a60e45966e14149727
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-04-21 16:08:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,7 @@ dados = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSiXIJe-xpj
 dados['Qual o peso?'] = pd.to_numeric(dados['Qual o peso?'].str.replace(',','.'))
 dados['Carimbo de data/hora'] = pd.to_datetime(dados['Carimbo de data/hora'])
 
-col1, col2 = st.columns(2)
-col1.metric('Peso médio Lisi', f"{round(dados[dados['Quem é você?']=='Lisi']['Qual o peso?'].mean(),1)} kg")
-col2.metric('Peso médio André', f"{round(dados[dados['Quem é você?']=='André']['Qual o peso?'].mean(),1)} kg")
+
 
 fig = px.line(dados, x='Carimbo de data/hora', y='Qual o peso?', color='Quem é você?', markers=True, title='Monitoramento de peso',
              labels={'Carimbo de data/hora': 'Data e hora', 'Qual o peso?': 'Peso (kg)'},
@@ -20,3 +18,6 @@ fig = px.line(dados, x='Carimbo de data/hora', y='Qual o peso?', color='Quem é 
              range_y=[0, 100])
 st.plotly_chart(fig, use_container_width=True, theme=None)
 
+col1, col2 = st.columns(2)
+col1.metric('Peso médio Lisi', f"{round(dados[dados['Quem é você?']=='Lisi']['Qual o peso?'].mean(),1)} kg")
+col2.metric('Peso médio André', f"{round(dados[dados['Quem é você?']=='André']['Qual o peso?'].mean(),1)} kg")
