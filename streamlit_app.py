2aff9ffbe0d00ba2bdaaf8349c1da143856ac8bb
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-04-21 15:51:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,14 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.set_page_config(
+    page_title="MOnitoramento peso",
+    layout="centered")
+
 dados = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSiXIJe-xpjsJCAG8RX38_QFNe9-b0RHmu0KbE7n942AQ7ssD9yyWHjc8vMBUD7pZLkwW_rP3E5nxUU/pub?output=csv')
 dados['Qual o peso?'] = pd.to_numeric(dados['Qual o peso?'].str.replace(',','.'))
 dados['Carimbo de data/hora'] = pd.to_datetime(dados['Carimbo de data/hora'])
 
 fig = px.line(dados, x='Carimbo de data/hora', y='Qual o peso?', color='Quem é você?', markers=True, title='Monitoramento de peso')
-st.plotly_chart(fig)
+st.plotly_chart(fig, use_container_width=True)
 
