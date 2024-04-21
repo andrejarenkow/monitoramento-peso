989121356f3f51f21c82ccb264812369ce2f415b
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-04-21 15:53:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ dados = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSiXIJe-xpj
 dados['Qual o peso?'] = pd.to_numeric(dados['Qual o peso?'].str.replace(',','.'))
 dados['Carimbo de data/hora'] = pd.to_datetime(dados['Carimbo de data/hora'])
 
-st.metric('Peso médio Lisi', dados[dados['Quem é você?']=='Lisi'].mean()
+st.metric('Peso médio Lisi', dados[dados['Quem é você?']=='Lisi'].mean())
 
 fig = px.line(dados, x='Carimbo de data/hora', y='Qual o peso?', color='Quem é você?', markers=True, title='Monitoramento de peso')
 st.plotly_chart(fig, use_container_width=True)
