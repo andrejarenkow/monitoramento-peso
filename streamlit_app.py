0a82684efa63cca8316669891a8c4c5200e8634b
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-04-21 15:56:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ dados['Qual o peso?'] = pd.to_numeric(dados['Qual o peso?'].str.replace(',','.')
 dados['Carimbo de data/hora'] = pd.to_datetime(dados['Carimbo de data/hora'])
 
 col1, col2 = st.columns(2)
-col1.metric('Peso médio Lisi', round(dados[dados['Quem é você?']=='Lisi']['Qual o peso?'].mean(),1)
+col1.metric('Peso médio Lisi', round(dados[dados['Quem é você?']=='Lisi']['Qual o peso?'].mean(),1))
 col2.metric('Peso médio André', round(dados[dados['Quem é você?']=='André']['Qual o peso?'].mean(),1))
 
 fig = px.line(dados, x='Carimbo de data/hora', y='Qual o peso?', color='Quem é você?', markers=True, title='Monitoramento de peso')
