69a6b42ffce2306a1e5eda163f505a3b02700e2e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-04-21 21:55:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ fig = px.line(dados, x='Carimbo de data/hora', y='Qual o peso?', color='Quem é 
 fig.update_layout(legend=dict(orientation='h', y=1.1, x=0.5, xanchor='center'))
 # Ajustando as margens para diminuir as bordas
 fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+fig.update_layout(hovermode=False)
 
 st.plotly_chart(fig, use_container_width=True, theme=None)
 
