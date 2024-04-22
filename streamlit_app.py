f5c051c3233a7a56384c657d2fe8fb966b8361c3
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-04-21 22:02:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,27 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
 
+# Configurações da página do Streamlit
 st.set_page_config(
     page_title="Monitoramento peso",
-    layout="centered")
+    layout="centered")  # Define o título da página e a layout centralizada
 
+# Carrega os dados do CSV online
 dados = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSiXIJe-xpjsJCAG8RX38_QFNe9-b0RHmu0KbE7n942AQ7ssD9yyWHjc8vMBUD7pZLkwW_rP3E5nxUU/pub?output=csv')
-dados['Qual o peso?'] = pd.to_numeric(dados['Qual o peso?'].str.replace(',','.'))
-dados['Carimbo de data/hora'] = pd.to_datetime(dados['Carimbo de data/hora'])
+dados['Qual o peso?'] = pd.to_numeric(dados['Qual o peso?'].str.replace(',','.'))  # Converte para numérico e substitui vírgula por ponto
+dados['Carimbo de data/hora'] = pd.to_datetime(dados['Carimbo de data/hora'])  # Converte para formato de data e hora
 
-
-
+# Cria o gráfico de linha com Plotly Express
 fig = px.line(dados, x='Carimbo de data/hora', y='Qual o peso?', color='Quem é você?', markers=True, 
-             labels={'Carimbo de data/hora': 'Data e hora', 'Qual o peso?': 'Peso (kg)'},
-             template='plotly_dark',
-             range_y=[0, 150])
+             labels={'Carimbo de data/hora': 'Data e hora', 'Qual o peso?': 'Peso (kg)'},  # Define os rótulos dos eixos
+             template='plotly_dark',  # Escolhe o tema escuro
+             range_y=[0, 150])  # Define o intervalo do eixo y
 
+# Ajusta a legenda para ser horizontal e centralizada
 fig.update_layout(legend=dict(orientation='h', y=1.1, x=0.5, xanchor='center'))
-# Ajustando as margens para diminuir as bordas
+# Ajusta as margens para diminuir as bordas
 fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
-#fig.update_layout(hovermode=False)
 
+# Exibe o gráfico no Streamlit, desabilitando todas as interações
 st.plotly_chart(fig, use_container_width=True, theme=None, config={})
-
-col1, col2 = st.columns(2)
-col1.metric('Média Lisi', f"{round(dados[dados['Quem é você?']=='Lisi']['Qual o peso?'].mean(),1)}")
-col2.metric('Média André', f"{round(dados[dados['Quem é você?']=='André']['Qual o peso?'].mean(),1)}")
